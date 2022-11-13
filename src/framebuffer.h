fdebdac5dbeeccf70f3e739ba32c2eb72bee9ac5
MaFackler <17232532+MaFackler@users.noreply.github.com>
2022-11-13 14:04:40 +0100

    EP10 - Stencil buffer

--- src/framebuffer.h
+++ src/framebuffer.h
@@ -1,5 +1,6 @@
 #ifndef FRAMEBUFFER_H
 #define FRAMEBUFFER_H
+#include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -10,7 +11,9 @@ typedef struct {
     uint32_t width;
     uint32_t height;
     uint32_t color;
+    uint8_t stencil_value;
     uint32_t *pixels;
+    uint8_t *stencil;
 } Framebuffer;
 
 typedef struct {
@@ -34,6 +37,9 @@ Framebuffer *framebuffer_create_external(uint32_t *pixels, uint32_t width, uint3
     res->pixels = pixels;
     res->width = width;
     res->height = height;
+    res->stencil = malloc(sizeof(uint8_t) * width * height);
+    res->stencil_value = 0xFF;
+    memset(res->stencil, res->stencil_value, width * height);
     return res;
 }
 
@@ -42,7 +48,19 @@ void framebuffer_fill_rect(Framebuffer *framebuffer, int x, int y, int w, int h)
     cliprect_clip(&rect, 0, 0, framebuffer->width, framebuffer->height);
     for (int y = rect.ymin; y < rect.ymax; ++y) {
         for (int x = rect.xmin; x < rect.xmax; ++x) {
-            framebuffer->pixels[y * framebuffer->width + x] = framebuffer->color;
+            if (framebuffer->stencil[y * framebuffer->width + x] > 0) {
+                framebuffer->pixels[y * framebuffer->width + x] = framebuffer->color;
+            }
+        }
+    }
+}
+
+void framebuffer_fill_rect_stencil(Framebuffer *framebuffer, int x, int y, int w, int h) {
+    ClipRect rect = {x, y, x + w, y + h};
+    cliprect_clip(&rect, 0, 0, framebuffer->width, framebuffer->height);
+    for (int y = rect.ymin; y < rect.ymax; ++y) {
+        for (int x = rect.xmin; x < rect.xmax; ++x) {
+            framebuffer->stencil[y * framebuffer->width + x] = framebuffer->stencil_value;
         }
     }
 }
@@ -56,7 +74,25 @@ void framebuffer_fill_circle(Framebuffer *framebuffer, int x0, int y0, int r) {
             float xf = (x - x0);
             float yf = (y - y0);
             if (xf * xf + yf * yf < rf) {
-                framebuffer->pixels[y * framebuffer->width + x] = framebuffer->color;
+                if (framebuffer->stencil[y * framebuffer->width + x] > 0) {
+                    framebuffer->pixels[y * framebuffer->width + x] = framebuffer->color;
+                }
+            }
+
+        }
+    }
+}
+
+void framebuffer_fill_circle_stencil(Framebuffer *framebuffer, int x0, int y0, int r) {
+    ClipRect rect = {x0 - r, y0 - r, x0 + 2 * r, y0 + 2 * r};
+    cliprect_clip(&rect, 0, 0, framebuffer->width, framebuffer->height);
+    float rf = r * r;
+    for (int y = rect.ymin; y < rect.ymax; ++y) {
+        for (int x = rect.xmin; x < rect.xmax; ++x) {
+            float xf = (x - x0);
+            float yf = (y - y0);
+            if (xf * xf + yf * yf < rf) {
+                framebuffer->stencil[y * framebuffer->width + x] = framebuffer->stencil_value;
             }
 
         }
@@ -83,7 +119,35 @@ void framebuffer_fill_triangle(Framebuffer *framebuffer, int ax, int ay, int bx,
             float u = (ax * p1 + p4 * p2 - x * p1) / (p3 * p2 - (bx - ax) * p1);
             float v = (p4 - u * p3) / p1;
             if (u >= 0.0f && v >= 0.0f && (u + v) <= 1.0f) {
-                framebuffer->pixels[y * framebuffer->width + x] = framebuffer->color;
+                if (framebuffer->stencil[y * framebuffer->width + x] > 0) {
+                    framebuffer->pixels[y * framebuffer->width + x] = framebuffer->color;
+                }
+            }
+        }
+    }
+}
+
+void framebuffer_fill_triangle_stencil(Framebuffer *framebuffer, int ax, int ay, int bx, int by, int cx, int cy) {
+    int xstart = MIN(MIN(ax, bx), cx);
+    int ystart = MIN(MIN(ay, by), cy);
+    int xend = MAX(MAX(ax, bx), cx);
+    int yend = MAX(MAX(ay, by), cy);
+    
+    ClipRect rect = {xstart, ystart, xend, yend};
+    cliprect_clip(&rect, 0, 0, framebuffer->width, framebuffer->height);
+
+    float p1 = cy - ay;
+    float p2 = cx - ax;
+    float p3 = by - ay;
+
+    for (int y = rect.ymin; y < rect.ymax; ++y) {
+        for (int x = rect.xmin; x < rect.xmax; ++x) {
+            float p4 = y - ay;
+
+            float u = (ax * p1 + p4 * p2 - x * p1) / (p3 * p2 - (bx - ax) * p1);
+            float v = (p4 - u * p3) / p1;
+            if (u >= 0.0f && v >= 0.0f && (u + v) <= 1.0f) {
+                framebuffer->stencil[y * framebuffer->width + x] = framebuffer->stencil_value;
             }
         }
     }
--- src/shared_loop.c
+++ src/shared_loop.c
@@ -4,9 +4,17 @@
 void update(Framebuffer *framebuffer) {
     framebuffer->color = 0xFFFFFFFF;
     framebuffer_fill_rect(framebuffer, 0, 0, framebuffer->width, framebuffer->height);
+    framebuffer->stencil_value = 0xFF;
+    framebuffer_fill_rect_stencil(framebuffer, 0, 0, framebuffer->width, framebuffer->height);
+
+    framebuffer->stencil_value = 0x00;
     framebuffer->color = 0xFFFF0000;
+    framebuffer_fill_rect_stencil(framebuffer, 10, 10, 80, 80);
     framebuffer_fill_rect(framebuffer, 0, 0, 100, 100);
 
+    framebuffer_fill_circle_stencil(framebuffer, 200, 200, 40);
     framebuffer_fill_circle(framebuffer, 200, 200, 50);
+
+    framebuffer_fill_triangle_stencil(framebuffer, 420, 410, 490, 410, 490, 480);
     framebuffer_fill_triangle(framebuffer, 400, 400, 500, 400, 500, 500);
 }
